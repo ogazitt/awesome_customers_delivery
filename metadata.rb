7f7de7ec48088c8674e3111b1d621162286397ee
Omri Gazitt <ogazitt@gmail.com>
2016-04-20 06:17:12 +0000

    Bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures awesome_customers_delivery'
 long_description 'Installs/Configures awesome_customers_delivery'
-version '0.1.0'
+version '0.2.0'
 
 depends 'chef-sugar'
 depends 'firewall', '~> 2.4.0'
